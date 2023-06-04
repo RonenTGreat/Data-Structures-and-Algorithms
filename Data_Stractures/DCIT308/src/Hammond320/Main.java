a0d8f11d6d453c219fae680b5ca68184ca93cb04
RonenTGreat <ronenh53@gmail.com>
2023-06-04 20:14:25 +0000

    .

--- Data_Stractures/DCIT308/src/Hammond320/Main.java
+++ Data_Stractures/DCIT308/src/Hammond320/Main.java
@@ -2,17 +2,20 @@ package Hammond320;
 
 public class Main {
     public static void main(String[] args) {
-        StackArrayList collection = new StackArrayList();
-        for(int i=1; i < 11; i++){
-            collection.push(i);
-        }
+        StackLinkedList<String> collection = new StackLinkedList<>();
 
-        System.out.println(collection.displayContent());
-        collection.push(1010);
-        System.out.println(collection.displayContent());
+        collection.push("Ronen");
+        collection.push("Ben");
+        collection.push("Ethel");
+        collection.push("Vanessa");
+        collection.push("William");
+        collection.displayContent();
+
+
         System.out.println(collection.pop());
         System.out.println(collection.isEmpty());
         System.out.println(collection.top());
+        collection.displayContent();
 
 
 
